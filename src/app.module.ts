e2e2a87962ad86acdd57bf292b196f03acd4af69
Dammycodes-all <harliarmeen@gmail.com>
2026-04-20 19:16:08 +0100

    finalize the removal of the store ad the classify service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ClassifyModule } from './classify/classify.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProfileModule } from './profile/profile.module';
@@ -7,7 +6,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ClassifyModule,
     ProfileModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
